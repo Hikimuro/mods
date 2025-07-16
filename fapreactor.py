8e8bce491e36b3d2a998fc10a60368f8b72609c1
Hikimuro <78354668+Hikimuro@users.noreply.github.com>
2025-07-16 18:40:55 +0400

    Update fapreactor.py

--- fapreactor.py
+++ fapreactor.py
@@ -13,10 +13,10 @@ import os
 logger = logging.getLogger(__name__)
 
 AVAILABLE_CATEGORIES = [
-    "PornOral", "PornVaginal", "PornToys", "Porngroup",
-    "PornLesbians", "PornHandjob", "PornFutanari", "Porn", "Hentai",
-    "shemale", "Newhalf", "footfetish", "Femdom", "изврат", "milf",
-    "anal", "teen", "bdsm", "yaoi", "yuri"
+    "Oral", "Vaginal", "Sex Toys", "Group",
+    "Lesbian", "Handjob", "Futanari", "Porn", "Hentai",
+    "Shemale", "Newhalf", "Footfetish", "Femdom", "Milf",
+    "Anal", "Teen", "Bdsm", "Yaoi", "Yuri", "Guro"
 ]
 
 @loader.tds
